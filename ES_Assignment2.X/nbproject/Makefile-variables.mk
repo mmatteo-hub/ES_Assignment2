5f8815fec25e451acc4beda87495d859168e4e46
ettore9x9 <ettoresani0@gmail.com>
2022-12-09 14:24:12 +0100

    setup

---
+++ ES_Assignment2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ES_Assignment2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ES_Assignment2.X.production.hex
